60d76033544e93335beacd6c8eb17cee0b2da475
RafaPorcel <rafaelporcelllobregat@gmail.com>
2026-03-15 13:33:57 +0100

    borrar carpeta subida por error

--- prueba1/src/main/java/com/example/prueba1/repository/EmpleadosTablasRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.prueba1.repository;
-
-import com.example.prueba1.model.EmpleadosTablas;
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmpleadosTablasRepository extends CrudRepository<EmpleadosTablas, Integer> {
-}
